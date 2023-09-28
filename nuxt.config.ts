6d5770bf4b8d85c17d7241908ac63f0cb5a63a1e
Sidney Alex <38598808+silvercent011@users.noreply.github.com>
2023-09-28 08:32:52 -0300

    feat(vee): add vee-validate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/devtools", "@bootstrap-vue-next/nuxt"],
+  modules: ["@nuxt/devtools", "@bootstrap-vue-next/nuxt", "@vee-validate/nuxt"],
   css: ["bootstrap/dist/css/bootstrap.min.css"],
 });
